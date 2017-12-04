65c9ca57be55e250f45f85a90c9c53d3d509b274
dr-matt-smith <dr_matt_smith@me.com>
2017-12-04 18:30:05 +0000

    added files to project

---
+++ Assets/Ducking/_Scripts/SoundTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// play audio source when collider trigger hit
+/// </summary>
+public class SoundTrigger : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // something has hit our collider trigger
+        // so start playing the sound
+        audioSource.Play();
+	}
+}
